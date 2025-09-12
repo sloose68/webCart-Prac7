efca00fddf7cb6375b664fa4923478dc69263c0f
Stuart Loose <stuart.loose@skillsforge.com.au>
2025-09-12 16:09:46 +0930

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,135 @@
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
+import ScaleBar from "@arcgis/core/widgets/ScaleBar";
+import Legend from "@arcgis/core/widgets/Legend";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+
+class MyMap {
+  constructor(containerId) {
+    this.containerId = containerId;
+  }
+
+  async init() {
+    const map = new Map({ basemap: "streets-navigation-vector" });
+
+    const view = new MapView({
+      container: this.containerId,
+      map: map,
+      center: [138.6007, -34.9285],
+      zoom: 12
+    });
+
+    view.when(() => {
+      this.addFeatureLayer(map);
+      this.addWidgets(view);
+    });
+  }
+
+  addFeatureLayer(map) {
+    // Feature data with extra fields for popup content
+    const features = [
+      {
+        geometry: { type: "point", longitude: 138.6007, latitude: -34.9285 },
+        attributes: {
+          ObjectID: 1,
+          Category: "Adelaide City Centre",
+          ImageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Adelaide_-_SA_%2825091207887%29.jpg",
+          Description: "Adelaide!"
+        }
+      },
+      {
+        geometry: { type: "point", longitude: 138.6050, latitude: -34.9200 },
+        attributes: {
+          ObjectID: 2,
+          Category: "Second Marker",
+          ImageUrl: "",
+          Description: "This is a second location with a distinct symbol."
+        }
+      },
+      {
+        geometry: { type: "point", longitude: 138.78360, latitude: -34.92896 },
+        attributes: {
+          ObjectID: 3,
+          Category: "Home",
+          ImageUrl: "",
+          Description: "192 Blockers Road!"
+        }
+      }
+    ];
+
+    const featureLayer = new FeatureLayer({
+      source: features,
+      objectIdField: "ObjectID",
+      fields: [
+        { name: "ObjectID", type: "oid" },
+        { name: "Category", type: "string" },
+        { name: "ImageUrl", type: "string" },
+        { name: "Description", type: "string" }
+      ],
+      renderer: {
+        type: "unique-value",
+        field: "Category",
+        uniqueValueInfos: [
+          {
+            value: "Adelaide City Centre",
+            symbol: {
+              type: "simple-marker",
+              color: "blue",
+              size: 12
+            },
+            label: "Adelaide City Centre"
+          },
+          {
+            value: "Second Marker",
+            symbol: {
+              type: "simple-marker",
+              color: "red",
+              size: 14,
+              style: "diamond"
+            },
+            label: "Second Marker"
+          },
+          {
+            value: "Home",
+            symbol: {
+              type: "simple-marker",
+              color: "yellow",
+              size: 20
+            },
+            label: "Home"
+          }
+        ]
+      },
+      popupTemplate: {
+        title: "{Category}",
+        content: `
+          <div>
+            {Description}<br>
+            <img src='{ImageUrl}' width='250'>
+          </div>
+        `
+      }
+    });
+
+    map.add(featureLayer);
+    this.featureLayer = featureLayer;
+  }
+
+  addWidgets(view) {
+    view.ui.add(new BasemapToggle({ view, nextBasemap: "satellite" }), "bottom-right");
+
+    view.ui.add(new ScaleBar({ view, unit: "metric" }), "bottom-left");
+
+    view.ui.add(
+      new Legend({
+        view,
+        layerInfos: [{ layer: this.featureLayer, title: "Point Markers" }]
+      }),
+      "top-right"
+    );
+  }
+}
+
+const mapApp = new MyMap("viewDiv");
+mapApp.init();
---
+++ src/map-view.js
@@ -0,0 +1,64 @@
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
+import ScaleBar from "@arcgis/core/widgets/ScaleBar";
+import Legend from "@arcgis/core/widgets/Legend";
+import Graphic from "@arcgis/core/Graphic";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
+
+class ArcGISMap extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `<style>#viewDiv { height: 100vh; }</style><div id="viewDiv"></div>`;
+  }
+
+  connectedCallback() {
+    const map = new Map({ basemap: "streets-navigation-vector" });
+    const view = new MapView({
+      container: this.shadowRoot.querySelector("#viewDiv"),
+      map: map,
+      center: [138.6007, -34.9285],
+      zoom: 12
+    });
+    
+    const graphicsLayer = new GraphicsLayer();
+    map.add(graphicsLayer);
+
+    view.when(() => {
+      view.ui.add(new BasemapToggle({ view, nextBasemap: "satellite" }), "bottom-right");
+      view.ui.add(new ScaleBar({ view, unit: "metric" }), "bottom-left");
+      view.ui.add(new Legend({
+        view,
+        layerInfos: [{ layer: graphicsLayer, title: "Point Markers" }]
+      }), "top-right");
+
+      graphicsLayer.addMany([
+        new Graphic({
+          geometry: { type: "point", longitude: 138.6007, latitude: -34.9285 },
+          symbol: new SimpleMarkerSymbol({ color: "blue", size: 12 }),
+          popupTemplate: {
+            title: "Adelaide CBD",
+            content: `<img src="" width="250"><br>Adelaide CBD!`
+          },
+          popupEnabled: true
+        }),
+        new Graphic({
+          geometry: { type: "point", longitude: 138.6050, latitude: -34.9200 },
+          symbol: new SimpleMarkerSymbol({ color: "red", size: 14, style: "diamond" }),
+          popupTemplate: {
+            title: "Second Marker",
+            content: "This is a second location with a distinct symbol."
+          }
+        })
+      ]);
+    });
+    this.shadowRoot.querySelector("#viewDiv").addEventListener("contextmenu", (e) => {
+      e.stopPropagation();
+      // Do NOT call e.preventDefault()
+    });
+  }
+}
+
+customElements.define("arcgis-map", ArcGISMap);
